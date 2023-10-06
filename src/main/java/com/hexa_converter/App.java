33c45dfc8b086e3cadc1551668ce9f140e68afb2
LorenzoPiombini <piombinilorenzo1987@gmail.com>
2023-10-06 16:35:37 -0400

    bug fixed and cleaned the code more

--- src/main/java/com/hexa_converter/App.java
+++ src/main/java/com/hexa_converter/App.java
@@ -8,11 +8,11 @@ public class App {
     public static void main(String[] args) {
 
         for (long i = 0; i < 3000; i++) {
-            // long i = 512l;
+            // long i = 32l;
             Base10ToHexConverter hexaConverter = new Base10ToHexConverter(i);
             System.out.println("decimal " + i + " = " + hexaConverter.getConvertedNumber() + " hexadecimal");
             try {
-                Thread.sleep(300);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
 
                 e.printStackTrace();
--- src/main/java/com/hexa_converter/Base10ToHexConverter.java
+++ src/main/java/com/hexa_converter/Base10ToHexConverter.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Base10ToHexConverter {
-    private long powIndex = 0l;
-    private long result = 0l;
+
     private String convertedNumber;
 
     public Base10ToHexConverter(long number) {
@@ -15,23 +14,6 @@ public class Base10ToHexConverter {
             this.convertedNumber = convertNumberInHexaLetters(getHexaValue(number));
         }
 
-    }
-
-    public long largestPowerOf16(long number) {
-
-        for (int i = 0; i < number; i++) {
-            if (((long) Math.pow((double) 16, (double) i)) > number) {
-                return result;
-            }
-            result = (long) Math.pow((double) 16, (double) i);
-            powIndex = (long) i;
-        }
-
-        return result;
-    }
-
-    public static long getRemmainder(long number, long largestPowerOf16) {
-        return number % largestPowerOf16;
     }
 
     public List<Long> getHexaValue(long numberToConvert) {
@@ -44,18 +26,20 @@ public class Base10ToHexConverter {
         }
 
         while (numberToConvert != 0) {
-            long hexaValue = numberToConvert / largestPowerOf16(numberToConvert);
-            hexavalues.add(hexaValue);
-            numberToConvert = getRemmainder(numberToConvert, largestPowerOf16(numberToConvert));
+            hexavalues.add(numberToConvert % 16);
+            numberToConvert = numberToConvert / 16;
         }
 
         return hexavalues;
     }
 
     public static String convertNumberInHexaLetters(List<Long> hexaValues) {
+
         StringBuilder sb = new StringBuilder();
 
-        for (Long h : hexaValues) {
+        for (int i = hexaValues.size() - 1; i >= 0; i--) {
+
+            long h = hexaValues.get(i);
 
             switch (Long.toString(h)) {
                 case "10":
@@ -81,8 +65,8 @@ public class Base10ToHexConverter {
                     sb.append(h);
                     break;
             }
-        }
 
+        }
         return sb.toString();
     }
 
