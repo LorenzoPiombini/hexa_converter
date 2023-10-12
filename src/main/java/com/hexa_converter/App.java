9627beaca233d0da19c90b4a2b44a684ead51e15
LorenzoPiombini <piombinilorenzo1987@gmail.com>
2023-10-12 18:25:39 -0400

    added performance computation

--- src/main/java/com/hexa_converter/App.java
+++ src/main/java/com/hexa_converter/App.java
@@ -6,13 +6,29 @@ package com.hexa_converter;
  */
 public class App {
     public static void main(String[] args) {
-        long n = -5059;
+        long start = System.currentTimeMillis();
+        long n = -5059009787676788988L;
+        simultaneousThread(n);
+        // sequetial(n);
+        long end = System.currentTimeMillis();
+        System.out.println("time :" + (end - start));
 
+    }
+
+    public static void simultaneousThread(long n) {
         ConversionExecution execution = new ConversionExecution(n);
 
         System.out.println("the number " + n + " in hexa:" + execution.getHexa() + "\nthe same number in binary is: "
                 + execution.getBinary() + ".");
 
+    }
+
+    public static void sequetial(long n) {
+        Base10ToBinaryConverter b = new Base10ToBinaryConverter(n);
+        Base10ToHexConverter h = new Base10ToHexConverter(n);
+
+        System.out.println("the number " + n + " in hexa:" + h.getConvertedNumber() + "\nthe same number in binary is: "
+                + b.getBinaryResult() + ".");
     }
 
 }
